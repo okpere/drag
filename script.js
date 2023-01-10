fe6d0b29cd31e33bfc979e57a8ae4b5681a15d93
okpere clinton <63456107+okpere@users.noreply.github.com>
2023-01-10 21:17:03 +0100

    newjs

--- script.js
+++ script.js
@@ -1,78 +1,99 @@
 let toggleContainer = document.querySelector(".toggle-container");
-    let hide = document.querySelector('.sleep')
-    let aside = document.querySelector('aside')
-    let add = document.querySelector('.add-task')
-    let modal = document.querySelector('.model')
-    // let toggle = toggleContainer.style.justifyContent;
+let hide = document.querySelector(".sleep");
+let nav = document.querySelector("nav");
+let add = document.querySelector(".add-task");
+let modal = document.querySelector(".model");
+// let toggle = toggleContainer.style.justifyContent;
 
-    function addtoggle (){
-        if(modal.style.display === "none"){
-            modal.style.display = "block" 
-        }else{
-            modal.style.display = "none"
-            
-        }
-    }
-    add.addEventListener('click', addtoggle)
-    function hideToogle(){
-        hide.classList.toggle('active')
-        document.body.classList.toggle('active')
-        aside.classList.toggle('active')
-    }
-    hide.addEventListener('click', hideToogle)
+let create = document.querySelector(".sn");
+let title = document.querySelector(".title-form");
+let description = document.querySelector(".description-form");
+let subtasks = document.querySelector(".subtasks-form");
+let opt = document.getElementById("naan");
 
+const doing = document.querySelector(".doing");
+const don = document.querySelector(".don");
+const todo = document.querySelector(".todo");
 
-    function changeToggle () {
-        toggleContainer.classList.toggle('active')
-        // if(toggleContainer.classList.toggle('active')){
-        //     document.body.style.color('black')
-        // }else{
-        //     document.body.style.color('white')
-        // }
-    }
-    toggleContainer.addEventListener("click", changeToggle);
-    let create = document.querySelector('.sn')
-    let title = document.querySelector('.title-form')
-    let description = document.querySelector('.description-form')
-    let subtasks = document.querySelector('.subtasks-form')
-    var opt = document.getElementById("naan");
-    let work = []
+let work = [];
 
+// events listeners
+add.addEventListener("click", addtoggle);
+hide.addEventListener("click", hideToogle);
+toggleContainer.addEventListener("click", changeToggle);
+create.addEventListener("click", createClick);
+// create.addEventListener("click", disp);
 
-    const doing = document.querySelector('.doing')
-    const don = document.querySelector('.don')
-    const todo = document.querySelector('.todo')
-
-
-
- create.addEventListener('click' , createClick)
- function createClick(e){
-    e.preventDefault()
-    let newtask = {
-    newTitle : title.value,
-    newDescription : description.value,
-    stat: opt.value
-    }
-    work.push(newtask)
-    localStorage.setItem('works', JSON.stringify(work));
+function addtoggle() {
+  if (modal.style.display === "none") {
+    modal.style.display = "block";
+  } else {
+    modal.style.display = "none";
+  }
 }
-create.addEventListener('click' , disp)
-function disp(){
-    JSON.parse(localStorage.getItem('works')).map(data => {
-        console.log(data.newTitle,data.stat)
-            if(data.stat === 'Todo'){
-                todo.innerHTML += `
-            <li>${data.newTitle}<li>`}
-            else if(data.stat === 'Doing'){
-                doing.innerHTML += `<li>${data.newTitle}<li>`   
-            }else if(data.stat === "Done"){
-                don.innerHTML += `<li>${data.newTitle}<li>`
-            }else{
+
+function hideToogle() {
+  // hide.classList.toggle("active");
+  // document.body.classList.toggle("active");
+  // nav.classList.toggle("active");
+  if(hide.style.display === "block"){
+    console.log('hi')
+
+  }else{
+    document.body.gridTemplateArea = "header header header","main main main"
+  }
+}
+
+function changeToggle() {
+  toggleContainer.classList.toggle("active");
+  // if(toggleContainer.classList.toggle('active')){
+  //     document.body.style.color('black')
+  // }else{
+  //     document.body.style.color('white')
+  // }
+}
+
+const getAllTasks = (work) => {
+  work = localStorage.getItem("works");
+};
+
+function createClick(e) {
+  e.preventDefault();
+  let newtask = {
+    newTitle: title.value,
+    newDescription: description.value,
+    stat: opt.value,
+  };
+
+  work.push(newtask);
+  console.log(work);
+  localStorage.setItem("works", JSON.stringify(work));
+
+  addWork(newtask);
+}
+
+function addWork(work) {
+  //create an li tag
+  const listItem = document.createElement("ol");
+  // create a text content
+  const text = document.createTextNode(work.newTitle);
+
+  //   add text content to the li tag
+  listItem.appendChild(text);
+  //   add li tag to todo, doing or don
+  if (work.stat === "Todo") {
+    // if work stat is 'Todo', add to todo list
+    todo.appendChild(listItem)
+  } else if (work.stat === "Doing") {
+    // if work stat is 'Doing', add to doing list
+    doing.appendChild(listItem)
+  } else {
+    // if work stat is 'Done', add to don list
+    don.appendChild(listItem)
+  }
+}
+
     
-            
-        }
-           
-    })
-} 
+
 
     
